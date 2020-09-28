270b4d8f6ce4dbb4d778a31a4c1c330e03ce5386
Алексей <alexburnitdown@gmail.com>
2020-09-28 11:29:06 +0300

    tabs DONE

--- js/popup.js
+++ js/popup.js
@@ -100,3 +100,23 @@ mapClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   mapPopup.classList.remove("modal-show");
 });
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        console.log('111');
+      const target = document.querySelector(tab.dataset.tabTarget)
+      tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active');
+      })
+      tabs.forEach(tab => {
+        tab.classList.remove('active');
+        tab.classList.remove('bg-white');
+      })
+      tab.classList.add('active');
+      tab.classList.add('bg-white');
+      target.classList.add('active');
+    })
+  })
